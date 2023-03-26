bbc71fc1d2c511b67f0590d876535da2a934a7c0
ilya knyazev <ilak9081@gmail.com>
2023-03-26 13:59:21 +0400

    p10-3r

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int gcd (int a, int b);
+int main () {
+	printf ("Test v10.03: OK\n");
+	int a = 12;
+	int b = 16;
+	printf ("a = %d\n", a);
+	printf ("b = %d\n", b);
+	printf ("NOD (%d, %d) = ", a, b);
+	a = gcd(a, b);
+	printf ("НОД (%d)", a);
+	return 0;
+}
+
+int gcd (int a, int b) {
+	while (a!=b) {
+		if (a>b) {
+			a -= b;
+		}
+		else {
+			b -= a;
+		}
+	}
+	return a;
+}
